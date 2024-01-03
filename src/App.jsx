459f99d4d9241361eb4c022bb4c000a1a73ef5c0
devMeno <charlesamenlokonon@gmail.com>
2024-01-03 23:57:10 +0100

    Changing header_file name to nav.jsx by header.jsx and adding nextui library

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './components/nav'
-
+import Header from './components/header'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <Nav />
+            <Header />
             <span className='underlined font-semibold text-4xl text-red-300'>Bonne nuit</span>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/theme");
 export default {
-     content: [
-          "./index.html",
-          "./src/**/*.{js,ts,jsx,tsx}",
-          "./node_modules/flowbite/**/*.js",
-     ],
+     content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
      theme: {
           extend: {},
      },
-     plugins: [require("flowbite/plugin")],
+     plugins: [nextui()],
 };
